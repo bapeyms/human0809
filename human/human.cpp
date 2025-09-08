42efb853de30742607b0171f37b1d382a0c20683
Serdy_ht74@student.itstep.org <Serdy_ht74@student.itstep.org>
2025-09-08 13:17:54 +0300

    new

--- human/human.cpp
+++ human/human.cpp
@@ -1,1 +1,44 @@
-#include "human.h"
+#include "human.h"// файл дизайну
+
+human::human()
+{
+}
+
+human::human(const char* n, const char* s, int a)
+{
+}
+
+void human::Init()
+{
+}
+
+void human::Print()
+{
+}
+
+char* human::GetName()
+{
+	return nullptr;
+}
+
+char* human::GetSurName()
+{
+	return nullptr;
+}
+
+int human::GetAge()
+{
+	return 0;
+}
+
+void human::SetName(const char* n)
+{
+}
+
+void human::SetSurname(const char* s)
+{
+}
+
+void human::SetAge(int a)
+{
+}
--- human/human.h
+++ human/human.h
@@ -1,5 +1,21 @@
-#pragma once
+#pragma once // פאיכ נואכ³חאצ³¿
 class human
 {
+	char* name;
+	char* surname;
+	int age;
+public:
+	human();
+	human(const char* n, const char* s, int a);
+	void Init();
+	void Print();
+
+	char* GetName();
+	char* GetSurName();
+	int GetAge();
+
+	void SetName(const char* n);
+	void SetSurname(const char* s);
+	void SetAge(int a);
 };
 
